9a50c0fca18e42dd2e419cac45cb8e91fb3889cc
GE ZHENG <dsjokerzz@gmail.com>
2017-08-20 13:51:01 +0900

    implementing code

--- matting.py
+++ matting.py
@@ -1,7 +1,10 @@
 import tensorflow as tf
 import numpy as np
 import random
-from scipy import misc
+from scipy import misc,ndimage
+import copy
+import itertools
+
 
 def unpool(pool, ind, ksize=[1, 2, 2, 1], scope='unpool'):
     
@@ -23,109 +26,149 @@ def unpool(pool, ind, ksize=[1, 2, 2, 1], scope='unpool'):
 		ret = tf.reshape(ret, output_shape)
 		return ret
 
-def preprocessing(sess,image_batch, trimap_batch, GTmatte_batch, batch_size = 128, image_size=320):
+def preprocessing(image_batch, GTmatte_batch, GTBG_batch, GTFG_batch, image_size=320):
 	'''
 	input: 
-		batch of image patch       
-		batch of corresponding trimap             suppose unknown region = 0.5
+		batch of image patch                  suppose unknown region = 128
 		batch of ground truth alpha matte
+		batch of BG
+		batch of FG
 	output:
-		batch of 4 channel data and corresponding alpha matte
+		batch of 4 channel data and corresponding alpha,FG,BG
 	'''
 
 	g_mean = np.array([123.68, 116.779, 103.939]).reshape([1,1,3])
 	image_batch_shape = image_batch.get_shape().as_list()
-
-	train_batch_pre = tf.concat([image_batch,trimap_batch,GTmatte_batch],3)
-	train_batch = np.ones([batch_size,image_size,image_size,5])
+	batch_size = image_batch_shape[0]
+	#generate trimap by random size (15~30) erosion and dilation
+	kernel = [val for val in range(10,31)]
+	trimap_batch = copy.deepcopy(GTmatte_batch)
+	for i in range(batch_size):
+		k_size = random.choice(kernel)
+		trimap_batch[i][np.where((ndimage.grey_dilation(GTmatte_batch[i],size=(k_size,k_size)) - ndimage.grey_erosion(GTmatte_batch[i],size=(k_size,k_size)))!=0)] = 128
+	trimap_batch = trimap_batch/255.0
+	
+	train_batch_pre = tf.concat([image_batch,trimap_batch,GTmatte_batch,GTBG_batch,GTFG_batch],3)
+	train_batch = np.zeros([batch_size,image_batch_shape[1],image_batch_shape[2],11])
 	for i in range(batch_size):
 		crop_size = random.choice([320,480,640])
 		flip = random.choice([0,1])
-		i_padding = center_padding(tf.slice(train_batch_pre,[i,0,0,0],[1,image_batch_shape[1],image_batch_shape[2],5]))
+		# i_padding = center_padding(sess,tf.slice(train_batch_pre,[i,0,0,0],[1,image_batch_shape[1],image_batch_shape[2],11]))
+		i_padding = tf.py_func(center_padding,[tf.slice(train_batch_pre,[i,0,0,0],[1,image_batch_shape[1],image_batch_shape[2],11])])
 		i_UR_center = UR_center(i_padding)
 		if crop_size == 320:
 			h_start_index = i_UR_center[0] - 159
 			w_start_index = i_UR_center[1] - 159
-			tmp = i_padding[h_start_index:h_start_index+320, w_start_index:w_start_index+320, 5]
+			tmp = i_padding[h_start_index:h_start_index+320, w_start_index:w_start_index+320, :]
 			if flip:
 				tmp = tmp[:,::-1,:]
-			tmp[:,:,:3] = tmp[:,:,:3] - mean
-			train_batch[i,:,:,:] = tmp.reshape([image_size,image_size,5])
+			# tmp[:,:,:3] = tmp[:,:,:3] - mean
+			train_batch[i,:,:,:] = tmp
 		if crop_size == 480:
 			h_start_index = i_UR_center[0] - 239
 			w_start_index = i_UR_center[1] - 239
-			tmp = i_padding[h_start_index:h_start_index+480, w_start_index:w_start_index+480, 5]
+			tmp = i_padding[h_start_index:h_start_index+480, w_start_index:w_start_index+480, :]
 			if flip:
 				tmp = tmp[:,::-1,:]
-			h1 = misc.imresize(tmp[:,:,:3],[image_size,image_size,3]) - mean
-			h2 = misc.imresize(tmp[:,:,3:],[image_size,image_size,2],interp = 'nearest')
-			train_batch[i,:,:,:] = np.concatenate([h1,h2],2)
+			tmp = misc.imresize(tmp,[image_size,image_size,11])
+			# tmp[:,:,:3] = tmp[:,:,:3] - mean
+			train_batch[i,:,:,:] = tmp
 		if crop_size == 640:
 			h_start_index = i_UR_center[0] - 319
 			w_start_index = i_UR_center[1] - 319
-			tmp = i_padding[h_start_index:h_start_index+640, w_start_index:w_start_index+640, 5]
+			tmp = i_padding[h_start_index:h_start_index+640, w_start_index:w_start_index+640, :]
 			if flip:
 				tmp = tmp[:,::-1,:]
-			h1 = misc.imresize(tmp[:,:,:3],[image_size,image_size,3]) - mean
-			h2 = misc.imresize(tmp[:,:,3:],[image_size,image_size,2],interp = 'nearest')
-			train_batch[i,:,:,:] = np.concatenate([h1,h2],2)
-	# else:
-	# 	#resize,去均值。 batch?
-	# 	#假设是batch
-	# 	test_batch_size = image_batch_shape[0]
-	# 	test_batch = np.ones([test_batch_size,image_size,image_size,5])
-	# 	test_batch_pre = tf.concat([image_batch,trimap_batch,GTmatte_batch],3)
-	# 	for i in range(test_batch):
-	# 		tf.slice(train_batch_pre,[i,0,0,0],[1,image_batch_shape[1],image_batch_shape[2],5])
+			tmp = misc.imresize(tmp,[image_size,image_size,11])
+			# tmp[:,:,:3] = tmp[:,:,:3] - mean
+			train_batch[i,:,:,:] = tmp
+	train_batch[:,:,:,:3] = train_batch[:,:,:,:3] - g_mean
+	train_batch[:,:,:,4:6] = (train_batch[:,:,:,4:6][np.where(train_batch[:,:,:,4:6] == 255)] + 1)/256
+	return train_batch[:,:,:,:4],train_batch[:,:,:,4],train_batch[:,:,:,5:8],train_batch[:,:,:,8:] #return input of CNN, and transformed GT alpha matte, GTBG,GTFG
+	
 
-
-
-
-def center_padding(sess,image):
+def center_padding(image):
 	'''
-	image consists 5 channel (images, trimap, GT alpha matte)
+	image consists 11 channel (images, trimap, GT alpha matte, GTBG_batch, GTFG_batch)
 	padding images to 2000*2000
 	'''
 
-	image_shape = image.get_shape().as_list()
+	# image_shape = image.get_shape().as_list()
+	# print(image_shape)
+	# h_center = (image_shape[0]-1)//2
+	# w_center = (image_shape[1]-1)//2
+	# pad_image = np.zeros([2000,2000,11])
+	# h_start_index = 999-h_center 
+	# h_end_index = h_start_index + image_shape[0]
+	# w_start_index = 999-w_center 
+	# w_end_index = w_start_index + image_shape[1]
+	# pad_image[h_start_index:h_end_index,w_start_index:w_end_index,:] = sess.run(image)
+	# return pad_image
+
+	image_shape = image.shape
 	print(image_shape)
 	h_center = (image_shape[0]-1)//2
 	w_center = (image_shape[1]-1)//2
-	pad_image = np.zeros([2000,2000,5])
-	h_start_index = 999-h_center 
+	pad_image = np.zeros([1300,1300,11])
+	h_start_index = 649-h_center 
 	h_end_index = h_start_index + image_shape[0]
-	w_start_index = 999-w_center 
+	w_start_index = 649-w_center 
 	w_end_index = w_start_index + image_shape[1]
-	pad_image[h_start_index:h_end_index,w_start_index:w_end_index,:] = sess.run(image)
+	pad_image[h_start_index:h_end_index,w_start_index:w_end_index,:] = image
 	return pad_image
 
 def UR_center(image):
 	'''
 	image consists 5 channel (images, trimap, GT alpha matte)
-	calculate center of unknown region
+	centered on unknown region
 	'''
-	UR = []
 	trimap = image[:,:,3]
-	for i in range(len(trimap)):
-		for j in range(len(trimap[i])):
-			if trimap[i,j]!=0 and trimap[i,j]!=1:
-				UR.append([i,j])
-	
-	return [int(i) for i in np.array(UR).mean(0)]
+	UR = [[i,j] for i, j in itertools.product(range(trimap.shape[0]), range(trimap.shape[1])) if trimap[i,j] == 128]
+#	return [int(i) for i in np.array(UR).mean(0)]
+	return random.choice(UR)
 
-
-
-def center_crop():
-	pass
-
+def composition_RGB(BG,FG,p_matte):
+	GB = tf.convert_to_tensor(BG)
+	FG = tf.convert_to_tensor(FG)
+	return p_matte * FG + (1 - p_matte) * BG
+			
 def global_mean():
 	pass
 
-def generate_trimaps():
-	pass
+def load_path(RGB,alpha,FG,BG):
+	RGBs_path = os.listdir(dataset_RGB)
+	RGBs_abspath = [os.path.join(dataset_RGB,RGB) for RGB in RGBs_path]
+	alphas_abspath = [os.path.join(alpha,RGB.split('-')[0]) for RGB in RGBs_path]
+	FGs_abspath = [os.path.join(FG,RGB.split('-')[0]) for RGB in RGBs_path]
+	BGs_abspath = [os.path.join(BG,RGB.split('-')[0],RGB.split('-')[1]) for RGB in RGBs_path]
+	return RGBs_abspath,alphas_abspath,FGs_abspath,BGs_abspath
 
-with tf.Session() as sess:
-	image = tf.Variable(tf.truncated_normal([10,10,5],dtype = tf.float32))
-	sess.run(tf.global_variables_initializer())
-	new_image = center_padding(sess,image)
-	print(UR_center(new_image))
+def load_data(batch_RGB_paths,batch_alpha_paths,batch_FG_paths,batch_BG_paths):
+	batch_RGBs = []
+	for path in batch_RGB_paths:
+		file_contents = tf.read_file(filename)
+		image = tf.image.decode_png(file_contents)
+		batch_RGBs.append(image)
+	batch_RGBs = tf.stack(batch_RGBs)
+
+	batch_alphas = []
+	for path in batch_alpha_paths:
+		file_contents = tf.read_file(filename)
+		image = tf.image.decode_jpg(file_contents)
+		batch_alphas.append(image)
+	batch_alphas = tf.stack(batch_alphas)
+
+	batch_FGs = []
+	for path in batch_FG_paths:
+		file_contents = tf.read_file(filename)
+		image = tf.image.decode_jpg(file_contents)
+		batch_FGs.append(image)
+	batch_FGs = tf.stack(batch_FGs)
+
+	batch_BGs = []
+	for path in batch_BG_paths:
+		file_contents = tf.read_file(filename)
+		image = tf.image.decode_jpg(file_contents)
+		batch_BGs.append(image)
+	batch_BGs = tf.stack(batch_BGs)
+	return batch_RGBs,batch_alphas,batch_FGs,batch_BGs
--- matting_deconv.py
+++ matting_deconv.py
@@ -1,28 +1,39 @@
 import tensorflow as tf
 import numpy as np
-from matting import unpool,preprocessing,composition_RGB
-
-
+from matting import unpool,preprocessing,composition_RGB,load_path,load_data
+import os
 
 image_size = 320
-batch_size = 128
+batch_size = 100
+max_epochs = 1000000
+
 #pretrained_vgg_model_path
 model_path = './vgg16_weights.npz'
-# image_batchs = tf.placeholder(tf.float32, shape=(batch_size,image_size,image_size,3))
-# trimap_batchs = tf.placeholder(tf.float32,shape=(batch_size,image_size,image_size,1))
 
-# #CNN_input = preprocessing(image_batchs,trimap_batchs)
-# #for test
-# CNN_input = tf.concat([image_batchs,trimap_batchs],axis = 3)
-image_batch = tf.placeholder(tf.float32, shape=(batch_size,image_size,image_size,3))
-trimap_batch = tf.placeholder(tf.float32,shape=(batch_size,image_size,image_size,1))
-GT_matte_batch = tf.placeholder(tf.float32, shape = (batch_size,image_size,image_size,1))
-GTBG_batch = tf.placeholder(tf.float32, shape = (batch_size,image_size,image_size,3))
-GTFG_batch = tf.placeholder(tf.float32, shape = (batch_size,image_size,image_size,3))
+dataset_RGB = './RGB'
+dataset_alpha = './alpha'
+dataset_FG = './FG'
+dataset_BG = './BG'
+
+paths_RGB,paths_alpha,paths_FG,paths_BG = load_path(dataset_RGB,dataset_alpha,dataset_FG,dataset_BG)
+range_size = len(paths_RGB)
+#range_size/batch_size has to be int
+batchs_per_epoch = int(range_size/batch_size) 
+
+index_queue = tf.train.range_input_producer(range_size, num_epochs=None,shuffle=True, seed=None, capacity=32)
+index_dequeue_op = index_queue.dequeue_many(batch_size, 'index_dequeue')
+
+image_batch = tf.placeholder(tf.float32, shape=(None,image_size,image_size,3))
+GT_matte_batch = tf.placeholder(tf.float32, shape = (None,image_size,image_size,1))
+GTBG_batch = tf.placeholder(tf.float32, shape = (None,image_size,image_size,3))
+GTFG_batch = tf.placeholder(tf.float32, shape = (None,image_size,image_size,3))
 en_parameters = []
-arg_unpooling = []
-b_input, b_GTmatte ,b_GTBG, b_GTFG= preprocessing(image_batch,GT_matte_batch,GTBG_batch,GTFG_batch,image_size)
+if training:
+    b_input, b_GTmatte ,b_GTBG, b_GTFG= preprocessing(image_batch,GT_matte_batch,GTBG_batch,GTFG_batch,image_size,)
+else:
+    preprocessing(image_batch)
 
+batch_size = image_batch.get_shape().as_list()[0]
 
 # conv1_1
 with tf.name_scope('conv1_1') as scope:
@@ -47,8 +58,7 @@ with tf.name_scope('conv1_2') as scope:
     en_parameters += [kernel, biases]
 
 # pool1
-pool1,argmax1 = tf.nn.max_pool_with_argmax(conv1_2,ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME',name='pool1')
-arg_unpooling.append(argmax1)
+pool1 = tf.nn.max_pool(conv1_2,ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME',name='pool1')
 
 # conv2_1
 with tf.name_scope('conv2_1') as scope:
@@ -73,8 +83,7 @@ with tf.name_scope('conv2_2') as scope:
     en_parameters += [kernel, biases]
 
 # pool2
-pool2,argmax2 = tf.nn.max_pool_with_argmax(conv2_2,ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME',name='pool2')
-arg_unpooling.append(argmax2)
+pool2 = tf.nn.max_pool(conv2_2,ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME',name='pool2')
 
 # conv3_1
 with tf.name_scope('conv3_1') as scope:
@@ -110,8 +119,7 @@ with tf.name_scope('conv3_3') as scope:
     en_parameters += [kernel, biases]
 
 # pool3
-pool3,argmax3 = tf.nn.max_pool_with_argmax(conv3_3,ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME',name='pool3')
-arg_unpooling.append(argmax3)
+pool3 = tf.nn.max_pool(conv3_3,ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME',name='pool3')
 
 # conv4_1
 with tf.name_scope('conv4_1') as scope:
@@ -147,8 +155,7 @@ with tf.name_scope('conv4_3') as scope:
     en_parameters += [kernel, biases]
 
 # pool4
-pool4,argmax4 = tf.nn.max_pool_with_argmax(conv4_3,ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME',name='pool4')
-arg_unpooling.append(argmax4)
+pool4 = tf.nn.max_pool(conv4_3,ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME',name='pool4')
 
 # conv5_1
 with tf.name_scope('conv5_1') as scope:
@@ -184,58 +191,70 @@ with tf.name_scope('conv5_3') as scope:
     en_parameters += [kernel, biases]
 
 # pool5
-pool5,argmax5 = tf.nn.max_pool_with_argmax(conv5_3,ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME',name='pool4')
-arg_unpooling.append(argmax5)
+pool5 = tf.nn.max_pool(conv5_3,ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME',name='pool4')
 
-#deconv6
-with tf.name_scope('deconv6') as scope:
+#deconv6_1
+with tf.name_scope('deconv6_1') as scope:
     outputs = tf.layers.conv2d_transpose(pool5, 512, [1, 1], strides=(1, 1), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
-    deconv6 = tf.nn.relu(outputs)
+    deconv6_1 = tf.nn.relu(outputs)
 
-#unpool1
-unpool1 = unpool(deconv6, arg_unpooling[-1], ksize=[1, 2, 2, 1], scope='unpool1')
+#deconv6_2
+with tf.name_scope('deconv6_2') as scope:
+    outputs = tf.layers.conv2d_transpose(deconv6_1, 512, [3, 3], strides=(2, 2), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
+    deconv6_2 = tf.nn.relu(outputs)
 
-#deconv5
-with tf.name_scope('deconv5') as scope:
-    outputs = tf.layers.conv2d_transpose(unpool1, 512, [5, 5], strides=(1, 1), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
-    deconv5 = tf.nn.relu(outputs)
 
-#unpool2
-unpool2 = unpool(deconv5, arg_unpooling[-2], ksize=[1, 2, 2, 1], scope='unpool2')
+#deconv5_1
+with tf.name_scope('deconv5_1') as scope:
+    outputs = tf.layers.conv2d_transpose(deconv6_2, 512, [5, 5], strides=(1, 1), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
+    deconv5_1 = tf.nn.relu(outputs)
 
-#deconv4
-with tf.name_scope('deconv4') as scope:
-    outputs = tf.layers.conv2d_transpose(unpool2, 256, [5, 5], strides=(1, 1), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
-    deconv4 = tf.nn.relu(outputs)
+#deconv5_2
+with tf.name_scope('deconv5_2') as scope:
+    outputs = tf.layers.conv2d_transpose(deconv5_1, 512, [3, 3], strides=(2, 2), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
+    deconv5_2 = tf.nn.relu(outputs)
 
-#unpool3
-unpool3 = unpool(deconv4, arg_unpooling[-3], ksize=[1, 2, 2, 1], scope='unpool3')
+#deconv4_1
+with tf.name_scope('deconv4_1') as scope:
+    outputs = tf.layers.conv2d_transpose(deconv5_2, 256, [5, 5], strides=(1, 1), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
+    deconv4_1= tf.nn.relu(outputs)
 
-#deconv3
-with tf.name_scope('deconv3') as scope:
-    outputs = tf.layers.conv2d_transpose(unpool3, 128, [5, 5], strides=(1, 1), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
-    deconv3 = tf.nn.relu(outputs)
+#deconv4_2
+with tf.name_scope('deconv4_2') as scope:
+    outputs = tf.layers.conv2d_transpose(deconv4_1, 256, [3, 3], strides=(2, 2), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
+    deconv4_2= tf.nn.relu(outputs)
 
-#unpool4
-unpool4 = unpool(deconv3, arg_unpooling[-4], ksize=[1, 2, 2, 1], scope='unpool4')
+#deconv3_1
+with tf.name_scope('deconv3_1') as scope:
+    outputs = tf.layers.conv2d_transpose(deconv4_2, 128, [5, 5], strides=(1, 1), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
+    deconv3_1 = tf.nn.relu(outputs)
 
-#deconv2
-with tf.name_scope('deconv2') as scope:
-    outputs = tf.layers.conv2d_transpose(unpool4, 64, [5, 5], strides=(1, 1), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
-    deconv2 = tf.nn.relu(outputs)
+#deconv3_2
+with tf.name_scope('deconv3_2') as scope:
+    outputs = tf.layers.conv2d_transpose(deconv3_1, 128, [3, 3], strides=(2, 2), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
+    deconv3_2 = tf.nn.relu(outputs)
 
-#unpool5
-unpool5 = unpool(deconv2, arg_unpooling[-5], ksize=[1, 2, 2, 1], scope='unpool5')
+#deconv2_1
+with tf.name_scope('deconv2_1') as scope:
+    outputs = tf.layers.conv2d_transpose(deconv3_2, 64, [5, 5], strides=(1, 1), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
+    deconv2_1 = tf.nn.relu(outputs)
+
+#deconv2_2
+with tf.name_scope('deconv2_2') as scope:
+    outputs = tf.layers.conv2d_transpose(deconv2_1, 64, [3, 3], strides=(2, 2), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
+    deconv2_2 = tf.nn.relu(outputs)
 
 #deconv1
 with tf.name_scope('deconv1') as scope:
-    outputs = tf.layers.conv2d_transpose(unpool5, 64, [5, 5], strides=(1, 1), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
+    outputs = tf.layers.conv2d_transpose(deconv2_2, 64, [5, 5], strides=(1, 1), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
     deconv1 = tf.nn.relu(outputs)
 
 #pred_alpha_matte
-with tf.name_scope('pred_alpha_matte') as scope:
+with tf.name_scope('pred_alpha') as scope:
     outputs = tf.layers.conv2d_transpose(deconv1, 1, [5, 5], strides=(1, 1), padding='SAME', kernel_initializer=tf.contrib.layers.xavier_initializer())
     pred_mattes = tf.nn.sigmoid(outputs)
+    #get binary predicted mattes
+    pred_mattes = tf.cast(tf.cast(pred_mattes + 0.5, tf.int32),tf.float32)
 
 with tf.name_scope('loss') as scope:
     alpha_diff = tf.square(b_GTmatte - pred_mattes)
@@ -251,27 +270,38 @@ with tf.name_scope('loss') as scope:
     total_loss = tf.reduce_sum(tf.convert_to_tensor(wl) * alpha_diff + tf.convert_to_tensor(2-wl) * c_diff) / batch_size
     train_op = tf.train.AdamOptimizer(learning_rate = 1e-5).minimize(total_loss)
 
-#b_input, b_GTmatte ,b_GTBG, b_GTFG= preprocessing(sess,image_batch,trimap_batch,GT_matte_batch,GTBG_batch,GTFG_batch,batch_size,image_size)
+with tf.Session() as sess:
+	sess.run(tf.global_variables_initializer())
+	batch_num = 0
+	epoch_num = 0
+	#initialize all parameters in vgg16
+	weights = np.load(model_path)
+	keys = sorted(weights.keys())
+	for i, k in enumerate(keys):
+		if i == 26:
+			break
+		if k == 'conv1_1_W':  
+			sess.run(en_parameters[i].assign(np.concatenate([weights[k],np.zeros([3,3,1,64])],axis = 2)))
+		else:
+			sess.run(en_parameters[i].assign(weights[k]))
 
-with sess.as_default():
-    sess.run(tf.global_variables_initializer())
-    #initialize all parameters in vgg16
-    weights = np.load(model_path)
-    keys = sorted(weights.keys())
-    for i, k in enumerate(keys):
-        if i == 26:
-            break
-        if k == 'conv1_1_W':  
-            sess.run(en_parameters[i].assign(np.concatenate([weights[k],np.zeros([3,3,1,64])],axis = 2)))
-        else:
-            sess.run(en_parameters[i].assign(weights[k]))
+	#load train data
+	while epoch_num < max_epochs:
+		print('epoch %d' % epoch_num)	
+		while batch_num < batchs_per_epoch:
+			print('batch %d' % batch_num)
+			batch_index = sess.run(index_dequeue_op)			
+		    batch_RGB_paths = np.array(paths_RGB)[index_epoch]
+		    batch_alpha_paths = np.array(paths_alpha)[index_epoch]
+		    batch_FG_paths = np.array(paths_FG)[index_epoch]
+		    batch_BG_paths = np.array(paths_BG)[index_epoch]
+		    
+		    batch_RGBs,batch_alphas,batch_FGs,batch_BGs = load_data(batch_RGB_paths,batch_alpha_paths,batch_FG_paths,batch_BG_paths)
 
-    for image_batch,trimap_batch,GTbatch:
-
-
-
-
-    
-
+			feed_dict = {image_batch:batch_RGBs,GT_matte_batch:batch_alphas,GTBG_batch:batch_BGs,GTFG_batch:batch_FGs}
+			_,loss = sess.run([train_op,total_loss],feed_dict = feed_dict)
+			print('loss %f \n' %loss)
+			batch_num += 1
+		epoch_num += 1
 
 
