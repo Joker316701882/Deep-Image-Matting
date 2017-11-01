5a3fceed232a14ac1e3ded8f47c8168eeada69b6
Александр Коншин <sashulya.konshin@gmail.com>
2017-11-01 17:56:22 +0300

    Fresh version of unpool added. It works with variable batch size.

--- matting.py
+++ matting.py
@@ -24,24 +24,28 @@ hard_samples = [
 ]
 
 def unpool(pool, ind, ksize=[1, 2, 2, 1], scope='unpool'):
-    
-	with tf.variable_scope(scope):
-		input_shape = pool.get_shape().as_list()
-		output_shape = (input_shape[0], input_shape[1] * ksize[1], input_shape[2] * ksize[2], input_shape[3])
+    with tf.variable_scope(scope):
+        input_shape = tf.shape(pool)
+        output_shape = [input_shape[0], input_shape[1] * ksize[1], input_shape[2] * ksize[2], input_shape[3]]
 
-		flat_input_size = np.prod(input_shape)
-		flat_output_shape = [output_shape[0], output_shape[1] * output_shape[2] * output_shape[3]]
+        flat_input_size = tf.reduce_prod(input_shape)
+        flat_output_shape = [output_shape[0], output_shape[1] * output_shape[2] * output_shape[3]]
 
-		pool_ = tf.reshape(pool, [flat_input_size])
-		batch_range = tf.reshape(tf.range(output_shape[0], dtype=ind.dtype), shape=[input_shape[0], 1, 1, 1])
-		b = tf.ones_like(ind) * batch_range
-		b = tf.reshape(b, [flat_input_size, 1])
-		ind_ = tf.reshape(ind, [flat_input_size, 1])
-		ind_ = tf.concat([b, ind_], 1)
+        pool_ = tf.reshape(pool, [flat_input_size])
+        batch_range = tf.reshape(tf.range(tf.cast(output_shape[0], tf.int64), dtype=ind.dtype), 
+                                          shape=[input_shape[0], 1, 1, 1])
+        b = tf.ones_like(ind) * batch_range
+        b1 = tf.reshape(b, [flat_input_size, 1])
+        ind_ = tf.reshape(ind, [flat_input_size, 1])
+        ind_ = tf.concat([b1, ind_], 1)
 
-		ret = tf.scatter_nd(ind_, pool_, shape=flat_output_shape)
-		ret = tf.reshape(ret, output_shape)
-		return ret
+        ret = tf.scatter_nd(ind_, pool_, shape=tf.cast(flat_output_shape, tf.int64))
+        ret = tf.reshape(ret, output_shape)
+
+        set_input_shape = pool.get_shape()
+        set_output_shape = [set_input_shape[0], set_input_shape[1] * ksize[1], set_input_shape[2] * ksize[2], set_input_shape[3]]
+        ret.set_shape(set_output_shape)
+        return ret
 
 
 def UR_center(trimap):
